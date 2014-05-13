40235eec8e4c7483953533235816bbc681309c56
Tim Leung <lzcgame@gmail.com>
2014-05-13 23:04:37 +0800

    qs模块使用示例

--- modules/qs.js
+++ modules/qs.js
@@ -1,3 +1,6 @@
+/**
+ * 把查询字符串解析为对象，或把对象解析为查询字符串
+ */
 var qs=require("qs");
 
 var reqStr="name=tim&body[foot]=foot&body[hand]=hand&queue[0]=0&queue[1]=1";
@@ -5,3 +8,5 @@ var reqStr="name=tim&body[foot]=foot&body[hand]=hand&queue[0]=0&queue[1]=1";
 var reqBody=qs.parse(reqStr);
 
 console.log(reqBody);
+
+console.log(qs.stringify(reqBody));
